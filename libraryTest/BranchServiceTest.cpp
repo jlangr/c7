fc755fd77d0d66889f402ae3ba43dce6fd1f2121
jlangr <jeff@langrsoft.com>
2012-10-31 13:55:23 -0600

    fix

--- libraryTest/BranchServiceTest.cpp
+++ libraryTest/BranchServiceTest.cpp
@@ -57,7 +57,7 @@ TEST_F(BranchServiceTest, AddThrowsWhenNameNotUnique)
 //START:CountZero
 TEST_F(BranchServiceTest, CountInitiallyZero)
 {
-    ASSERT_THAT("branch count should be 0", service.BranchCount(), Eq(0));
+    ASSERT_THAT(service.BranchCount(), Eq(0));
 }
 //END:CountZero
 
