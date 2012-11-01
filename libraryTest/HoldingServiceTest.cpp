7e977042a77d93a92b73a5704d19975191234630
jlangr <jeff@langrsoft.com>
2012-11-01 09:25:26 -0600

    AAA

--- libraryTest/HoldingServiceTest.cpp
+++ libraryTest/HoldingServiceTest.cpp
@@ -259,14 +259,16 @@ TEST_F(HoldingServiceTest, CheckInEarlyDoesNotUpdatePatronFineBalance)
     ASSERT_THAT(FindPatronWithId(patronCardNumber).FineBalance(), Eq(0));
 }
 
-//START:NoAAA
+//START:AAA
 TEST_F(HoldingServiceTest, X)
 {
     HoldingBarcode barcode(THE_TRIAL_CLASSIFICATION, 1);
     string patronCardNumber("p5");
     CheckOut(barcode, branch1, patronCardNumber);
     date_duration oneDayLate(Book::BOOK_CHECKOUT_PERIOD + 1);
+
     holdingService.CheckIn(barcode.AsString(), *arbitraryDate + oneDayLate, branch2->Id());
+
     ASSERT_THAT(FindPatronWithId(patronCardNumber).FineBalance(), Eq(Book::BOOK_DAILY_FINE));
 }
-//END:NoAAA
+//END:AAA
