a53c453dd3ddeb4f8158053c66744644a4f49056
jlangr <jeff@langrsoft.com>
2013-05-06 16:39:05 -0600

    assert fix

--- libraryTest/BranchServiceTest.cpp
+++ libraryTest/BranchServiceTest.cpp
@@ -68,8 +68,8 @@ TEST_F(BranchServiceTest, DeleteAllRemovesAnyAddedBranches)
 
     BranchService::DeleteAll();
 
-    ASSERT_THAT(service.Find(*eastBranch), Eq(false));
-    ASSERT_THAT(service.Find(*westBranch), Eq(false));
+    ASSERT_FALSE(service.Find(*eastBranch));
+    ASSERT_FALSE(service.Find(*westBranch));
 }
 
 TEST_F(BranchServiceTest, DeleteAllSetsCountToZero)
@@ -84,14 +84,14 @@ TEST_F(BranchServiceTest, DeleteAllSetsCountToZero)
 
 TEST_F(BranchServiceTest, FindAnswersFalseForNonexistentBranch)
 {
-    ASSERT_THAT(service.Find(*eastBranch), Eq(false));
+    ASSERT_FALSE(service.Find(*eastBranch));
 }
 
 TEST_F(BranchServiceTest, FindAnswersTrueForAddedBranch)
 {
     service.Add(*eastBranch);
     
-    ASSERT_THAT(service.Find(*eastBranch), Eq(true));
+    ASSERT_TRUE(service.Find(*eastBranch));
 }
 
 TEST_F(BranchServiceTest, FindRetrievesById)
@@ -118,6 +118,6 @@ TEST_F(BranchServiceTest, PersistsAcrossServiceInstances)
     service.Add(*eastBranch);
 
     BranchService anotherServiceInstance;
-    ASSERT_THAT(anotherServiceInstance.Find(*eastBranch), Eq(true));
+    ASSERT_TRUE(anotherServiceInstance.Find(*eastBranch));
     ASSERT_THAT(anotherServiceInstance.BranchCount(), Eq(1));
 }
