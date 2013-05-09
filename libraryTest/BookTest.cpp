df7a73504d98fc591f74b25ed13f49b26f64c4d0
jlangr <jeff@langrsoft.com>
2013-05-09 10:07:36 -0600

    tabs and assert fix

--- libraryTest/BookTest.cpp
+++ libraryTest/BookTest.cpp
@@ -11,78 +11,78 @@ using namespace ClassificationData;
 
 TEST(BookTest, PopulatesAllFieldsOnConstruction)
 {
-	Book book("title", "author", 1999, "classification");
+   Book book("title", "author", 1999, "classification");
 
-	ASSERT_THAT(book.Title(), Eq("title"));
-	ASSERT_THAT(book.Author(), Eq("author"));
-	ASSERT_THAT(book.Year(), Eq(1999));
-	ASSERT_THAT(book.Classification(), Eq("classification"));
-	ASSERT_THAT(book.Type(), Eq(Book::TYPE_BOOK));
+   ASSERT_THAT(book.Title(), Eq("title"));
+   ASSERT_THAT(book.Author(), Eq("author"));
+   ASSERT_THAT(book.Year(), Eq(1999));
+   ASSERT_THAT(book.Classification(), Eq("classification"));
+   ASSERT_THAT(book.Type(), Eq(Book::TYPE_BOOK));
 }
 
-TEST(BookTest, NoArgConstructorInitializesFieldsToDefault)
+TEST(BookTest, DefaultConstructorInitializesFieldsToDefault)
 {
-	Book book;
+   Book book;
 
-    ASSERT_THAT(book.Title(), Eq(""));
-	ASSERT_THAT(book.Author(), Eq(""));
-	ASSERT_THAT(book.Year(), Eq(0));
-	ASSERT_THAT("", Eq(book.Classification()));
+   ASSERT_THAT(book.Title(), Eq(""));
+   ASSERT_THAT(book.Author(), Eq(""));
+   ASSERT_THAT(book.Year(), Eq(0));
+   ASSERT_THAT(book.Classification(), Eq(""));
 }
 
 TEST(BookTest, AllowsSpecificationOfTypeOnConstruction)
 {
-	Book book("", "", 0, "", Book::TYPE_MOVIE);
+   Book book("", "", 0, "", Book::TYPE_MOVIE);
 
-	ASSERT_THAT(book.Type(), Eq(Book::TYPE_MOVIE));
+   ASSERT_THAT(book.Type(), Eq(Book::TYPE_MOVIE));
 }
 
 TEST(BookTest, IsEqualToCopyConstructedInstance)
 {
-	Book copy(THE_TRIAL);
+   Book copy(THE_TRIAL);
 
-    ASSERT_TRUE(THE_TRIAL == copy);
+   ASSERT_TRUE(THE_TRIAL == copy);
 }
 
 TEST(BookTest, IsNotEqualWhenTitleDiffers)
 {
-	Book book(
-		THE_TRIAL_TITLE + "x", THE_TRIAL_AUTHOR, 
-		THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION);
-	ASSERT_TRUE(THE_TRIAL != book);
+   Book book(
+   	THE_TRIAL_TITLE + "x", THE_TRIAL_AUTHOR, 
+   	THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION);
+   ASSERT_TRUE(THE_TRIAL != book);
 }
     
 TEST(BookTest, IsNotEqualWhenAuthorDiffers)
 {
-	Book book(
-		THE_TRIAL_TITLE, THE_TRIAL_AUTHOR + "x", 
-		THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION);
-	ASSERT_TRUE(THE_TRIAL != book);
+   Book book(
+   	THE_TRIAL_TITLE, THE_TRIAL_AUTHOR + "x", 
+   	THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION);
+   ASSERT_TRUE(THE_TRIAL != book);
 }
 
 TEST(BookTest, IsNotEqualWhenYearDiffers)
 {
-	Book book(
-		THE_TRIAL_TITLE, THE_TRIAL_AUTHOR, 
-		THE_TRIAL_YEAR + 1, THE_TRIAL_CLASSIFICATION);
-	ASSERT_TRUE(THE_TRIAL != book);
+   Book book(
+   	THE_TRIAL_TITLE, THE_TRIAL_AUTHOR, 
+   	THE_TRIAL_YEAR + 1, THE_TRIAL_CLASSIFICATION);
+   ASSERT_TRUE(THE_TRIAL != book);
 }
 
 TEST(BookTest, IsNotEqualWhenClassificationDiffers)
 {
-	Book book(
-		THE_TRIAL_TITLE, THE_TRIAL_AUTHOR, 
-		THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION + "x");
-	ASSERT_TRUE(THE_TRIAL != book);
+   Book book(
+   	THE_TRIAL_TITLE, THE_TRIAL_AUTHOR, 
+   	THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION + "x");
+   ASSERT_TRUE(THE_TRIAL != book);
 }
 
 TEST(BookTest, IsNotEqualWhenTypeDiffers)
 {
-	Book book6(
-		THE_TRIAL_TITLE, THE_TRIAL_AUTHOR, 
-		THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION, 
-		Book::TYPE_MOVIE);
-	ASSERT_TRUE(THE_TRIAL != book6);
+   Book book6(
+   	THE_TRIAL_TITLE, THE_TRIAL_AUTHOR, 
+   	THE_TRIAL_YEAR, THE_TRIAL_CLASSIFICATION, 
+   	Book::TYPE_MOVIE);
+   ASSERT_TRUE(THE_TRIAL != book6);
 }
 
 TEST(BookTest, LessThanBasedOnClassification)
